9272b9a33b1e8f83b7dadcc8d306f27a975729f4
Himanshu Panwar <hpanwar875@gmail.com>
2025-12-27 09:32:08 +0530

    feat : importing port from env file & added dotenv configuration

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,10 +2,14 @@ import express from "express"
 const app = express()
 import cookieParser from "cookie-parser"
 import { Request,Response } from "express";
+import dotenv from "dotenv"
+import cors from "cors"
 
-const port = 3000;
+dotenv.config()
 
+const port = process.env.PORT || "5000"
 
+app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
 
