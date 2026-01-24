d25467e32498ea8799db82f2530434c232f6fe90
Himanshu Panwar <hpanwar875@gmail.com>
2026-01-24 21:49:58 +0530

    feat: add JWT utility function for user authentication

---
+++ server/src/utils/jwt.ts
@@ -0,0 +1,13 @@
+import  jwt  from "jsonwebtoken";
+import type {UserModel} from "../generated/prisma/models/User.js"
+
+export function generateUserJWT(user: UserModel): string {
+  const secretKey = process.env.JWT_SECRET;
+  
+  if (!secretKey) {
+    throw new Error("JWT_SECRET is required");
+  }
+  
+  const token = jwt.sign({ id: user.id }, secretKey, { expiresIn: "7d" });
+  return token;
+}
