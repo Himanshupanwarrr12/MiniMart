454793cc3d4a5099ab36ea283cd50b8b5b43cf39
Himanshu Panwar <hpanwar875@gmail.com>
2026-01-27 20:44:40 +0530

    feat : auth with signUp route

--- server/src/routes/auth.ts
+++ server/src/routes/auth.ts
@@ -1,7 +1,7 @@
 import express from "express"
 const authRouter = express.Router()
-import type { Request,Response } from "express"
 
-authRouter.post("/signUp",(req:Request,res:Response)=>{
 
-})
+authRouter.post("/signUp",)
+
+module.exports= {authRouter}
