5a5c4b4be603fd5a5b10188980f140426f8e72af
Himanshu Panwar <hpanwar875@gmail.com>
2026-02-07 22:34:12 +0530

    change import route

--- server/src/routes/product.ts
+++ server/src/routes/product.ts
@@ -1,5 +1,5 @@
 import express from "express"
-import { products } from "../controllers/product.js"
+import { products } from "../controllers/productController.js"
 const profileRouter = express.Router()
 
 profileRouter.get("/products",products)
