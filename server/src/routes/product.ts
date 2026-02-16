0b415c23bf8fcf0d687803093647f719433111b8
Himanshu Panwar <hpanwar875@gmail.com>
2026-02-16 11:58:41 +0530

    added prodcut detail route

--- server/src/routes/product.ts
+++ server/src/routes/product.ts
@@ -1,9 +1,10 @@
 import express from "express"
-import { createProduct, getAllProducts } from "../controllers/productController.js"
+import { createProduct, getAllProducts, getProductDetails } from "../controllers/productController.js"
 import { authenticateToken, isAdmin } from "../middleware/auth.js"
 const productRouter = express.Router()
 
 productRouter.get("/products",getAllProducts)
+productRouter.get("/products/:id",getProductDetails)
 productRouter.post("/admin/products",authenticateToken,isAdmin,createProduct)
 
 export default productRouter
