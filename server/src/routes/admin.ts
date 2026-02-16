cbfa20405825055eb5740ac24addd20533168bd3
Himanshu Panwar <hpanwar875@gmail.com>
2026-02-16 12:34:49 +0530

    added admin APIs

---
+++ server/src/routes/admin.ts
@@ -0,0 +1,20 @@
+// src/routes/admin.ts
+import express from "express";
+import {
+  getDashboard,
+  getAllOrders,
+  updateOrderStatus,
+} from "../controllers/adminController.js";
+import { authenticateToken, isAdmin } from "../middleware/auth.js";
+
+const adminRouter = express.Router();
+
+adminRouter.use(authenticateToken);
+adminRouter.use(isAdmin);
+
+adminRouter.get("/admin/dashboard", getDashboard);
+
+adminRouter.get("/admin/orders", getAllOrders);
+adminRouter.put("/admin/orders/:id/status", updateOrderStatus);
+
+export default adminRouter;
