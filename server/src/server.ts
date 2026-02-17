227da80215285404799739e4d3b9ed6c1f86099e
Himanshu Panwar <hpanwar875@gmail.com>
2026-02-17 21:13:29 +0530

    added addressRouter & checkoutRouter

--- server/src/server.ts
+++ server/src/server.ts
@@ -10,18 +10,21 @@ app.use(cors())
 app.use(express.json())
 app.use(cookieParser())
 
-//api routes
 import authRouter from "./routes/auth.js";
 import productRouter from "./routes/product.js";
 import cartRouter from "./routes/cart.js";
 import orderRouter from "./routes/order.js";
 import adminRouter from "./routes/admin.js";
+import addressRouter from "./routes/address.js";
+import checkoutRouter from "./routes/checkout.js";
 
 app.use("/",authRouter)
 app.use("/",productRouter)
 app.use("/",cartRouter)
 app.use("/",orderRouter)
 app.use("/",adminRouter)
+app.use("/",addressRouter)
+app.use("/",checkoutRouter)
 
 app.get("/",(req:Request,res:Response)=>{
     res.send("Express server is running")
