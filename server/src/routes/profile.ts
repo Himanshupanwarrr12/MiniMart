f813c6e8d7660f10f96dde5eecd1a4f0c8317f30
Himanshu Panwar <hpanwar875@gmail.com>
2026-03-06 23:50:02 +0530

    add getProfile route

--- server/src/routes/profile.ts
+++ server/src/routes/profile.ts
@@ -1,8 +1,9 @@
 import express from "express"
 import { authenticateToken } from "../middleware/auth.js"
+import { getProfile } from "../controllers/profileController.js"
 
 const profileRouter = express.Router()
 
-profileRouter.get("/profile",authenticateToken,)
+profileRouter.get("/profile",authenticateToken,getProfile)
 
 export default profileRouter
