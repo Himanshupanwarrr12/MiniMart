03bd1344d6d4f512e45aa673165158c22952803e
Himanshu Panwar <hpanwar875@gmail.com>
2026-03-12 23:38:24 +0530

    fixed trycatch block & dispatch user to rtk store

--- client/src/AppLayout.tsx
+++ client/src/AppLayout.tsx
@@ -1,19 +1,28 @@
-import { Outlet } from "react-router-dom";
+import { Outlet} from "react-router-dom";
 import Navbar from "./ui/Navbar";
 import { useEffect } from "react";
 import axiosInstance from "./utils/axios.config";
+import { useDispatch } from "react-redux";
+import { addUser } from "./store/slices/userSlice";
 
 const AppLayout = () => {
+  const dispatch = useDispatch()
+
   useEffect(() => {
-    try {
-      const fetchProfile = async () => {
-        const data = await axiosInstance.get("/profile");
-        console.log("data : ",data)
-      };
-      fetchProfile();
-    } catch (error) {
-      console.log("error during fetch profile", error);
-    }
+    const storedUser = localStorage.getItem("user")
+    if (storedUser) dispatch(addUser(JSON.parse(storedUser)))
+
+    const fetchProfile = async () => {
+      try {                              
+        const { data } = await axiosInstance.get("/profile");
+        const user = data.data
+        dispatch(addUser(user))
+        localStorage.setItem("user", JSON.stringify(user)) 
+      } catch (error) {
+        console.log("error during fetch profile", error);
+      }
+    };
+    fetchProfile();
   }, []);
 
   return (
@@ -25,5 +34,4 @@ const AppLayout = () => {
     </div>
   );
 };
-
 export default AppLayout;
