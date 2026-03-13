54ddf3808c818023477db75df665944efae3356d
Himanshu Panwar <hpanwar875@gmail.com>
2026-03-13 23:32:04 +0530

    fix: handle corrupted localStorage user data in AppLayout

--- client/src/AppLayout.tsx
+++ client/src/AppLayout.tsx
@@ -1,4 +1,4 @@
-import { Outlet} from "react-router-dom";
+import { Outlet } from "react-router-dom";
 import Navbar from "./ui/Navbar";
 import { useEffect } from "react";
 import axiosInstance from "./utils/axios.config";
@@ -6,18 +6,23 @@ import { useDispatch } from "react-redux";
 import { addUser } from "./store/slices/userSlice";
 
 const AppLayout = () => {
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    const storedUser = localStorage.getItem("user")
-    if (storedUser) dispatch(addUser(JSON.parse(storedUser)))
+    try {
+      const storedUser = localStorage.getItem("user");
+      if (storedUser) dispatch(addUser(JSON.parse(storedUser)));
+    } catch (error: unknown) {
+      console.warn("Invalid user data in localStorage, clearing...", error);
+      localStorage.removeItem("user");
+    }
 
     const fetchProfile = async () => {
-      try {                              
+      try {
         const { data } = await axiosInstance.get("/profile");
-        const user = data.data
-        dispatch(addUser(user))
-        localStorage.setItem("user", JSON.stringify(user)) 
+        const user = data.data;
+        dispatch(addUser(user));
+        localStorage.setItem("user", JSON.stringify(user));
       } catch (error) {
         console.log("error during fetch profile", error);
       }
