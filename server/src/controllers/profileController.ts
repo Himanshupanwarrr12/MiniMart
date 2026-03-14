cd29f3cd5becdbbf7df24517aa4ee6e918a5ebe7
Himanshu Panwar <hpanwar875@gmail.com>
2026-03-14 23:16:48 +0530

    add update profile route

--- server/src/controllers/profileController.ts
+++ server/src/controllers/profileController.ts
@@ -41,3 +41,60 @@ export const getProfile = async (
     });
   }
 };
+
+export const updateProfile = async (req:AuthRequest,res:Response) : Promise<void>=>{
+
+  try {
+    if(!req.user){
+      res.status(401).json({
+        success:false,
+        error: "User not authenticated"
+      })
+      return
+    }
+
+    const {firstName,lastName,email} = req.body;
+
+    if (!firstName && !lastName && !email) {
+      res.status(400).json({
+        success: false,
+        error: "At least one field (firstName, lastName, email) must be provided",
+      });
+      return;
+    }
+
+    if (email) {
+      const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+      if (!emailRegex.test(email)) {
+        res.status(400).json({
+          success: false,
+          error: "Invalid email format",
+        });
+        return;
+      }
+    }
+
+    if (firstName !== undefined && firstName.trim() === "") {
+      res.status(400).json({
+        success: false,
+        error: "First name cannot be empty",
+      });
+      return;
+    }
+
+    const profile = await profileService.updateUserProfile(req.user.id ,{firstName,lastName,email})
+
+    res.status(200).json({
+      success:true,
+      message:"Update profile successfully",
+      data: profile,
+    })
+    
+  } catch (error) {
+     console.error("Error updating profile:", error);
+    res.status(500).json({
+      success: false,
+      error: error instanceof Error ? error.message : "Internal server error",
+    });
+  }
+}
