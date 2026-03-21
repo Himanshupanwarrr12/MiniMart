dd040f4bf432b9dc0286c75d4df18ae57ab63df2
Himanshu Panwar <hpanwar875@gmail.com>
2026-03-21 22:22:58 +0530

    add address & orders slices

--- client/src/store/store.ts
+++ client/src/store/store.ts
@@ -2,12 +2,16 @@ import { configureStore } from "@reduxjs/toolkit";
 import productReducer from "./slices/productSlice"
 import cartReducer from "./slices/cartSlice"
 import userReducer from "./slices/userSlice"
+import addressReducer from "./slices/addressSlice"
+import orderReducer from  "./slices/orderSlice"
 
 const store = configureStore({
     reducer:{
         user : userReducer,
         products : productReducer,
         cart : cartReducer,
+        address:addressReducer,
+        orders:orderReducer
     }
 })
 
